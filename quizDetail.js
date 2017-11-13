2ca127abcd159e1a1e41fb335708f2467c71b8d5
rawalsakshi <33219675+rawalsakshi@users.noreply.github.com>
2017-11-13 12:07:23 +0530

    Update quizDetail.js

--- quizDetail.js
+++ quizDetail.js
@@ -1,11 +1,11 @@
 function startQuiz(){
   document.getElementById("quiz").innerHTML=`<div id="startPage">
-                                                <h2>Ready for Fun Quiz!!</h2>
-                                                <h2 style="padding-top: 2vh">Click on button to start Quiz</h2>
-                                                <button id="mainButton" onclick="QuizStarted()" style="margin-top:12vh">Start Quiz</button>
-                                                <p id="note">Note: Quiz contains 15 questions.</p>
-                                                <p id="note"> &nbsp; &nbsp; &nbsp; Quiz Timmmings : 2 mins</p>
-                                              </div>`;
+    <h2>Ready for Fun Quiz!!</h2>
+    <h2 style="padding-top: 2vh">Click on button to start Quiz</h2>
+    <button id="mainButton" onclick="QuizStarted()" style="margin-top:12vh">Start Quiz</button>
+    <p id="note">Note: Quiz contains 15 questions.</p>
+    <p id="note"> &nbsp; &nbsp; &nbsp; Quiz Timmmings : 2 mins</p>
+  </div>`;
   document.getElementById("previous").style.display = "none";
   document.getElementById("next").style.display = "none";
   document.getElementById("submit").style.display = "none";
@@ -37,19 +37,19 @@ function QuizStarted(){
 
     function buildQuiz() {
       var output = [];
-
+      
       myQuestions.forEach((currentQuestion, questionNumber) => {
         var answers = [];
 
         for (letter in currentQuestion.answers) {
           answers.push(
             `<div>
-            <label class="custom-control custom-radio">
-               <input type="radio" class="custom-control-input" name="question${questionNumber}" value="${letter}">
-               <span class="custom-control-indicator"></span>
-                <span class="custom-control-description">
-                ${currentQuestion.answers[letter]}</span>
-            </label>
+              <label class="custom-control custom-radio">
+                 <input type="radio" class="custom-control-input" name="question${questionNumber}" value="${letter}">
+                 <span class="custom-control-indicator"></span>
+                  <span class="custom-control-description">
+                  ${currentQuestion.answers[letter]}</span>
+              </label>
             </div>`
           );
         }
@@ -81,31 +81,30 @@ function QuizStarted(){
 
       if(numCorrect<7){
         quizContainer.innerHTML = `<div id="resultDisplay">
-                                    <div class="card" style="width: 30rem; height:15rem">
-                                      <div class="card-header">
-                                        <h2>Result</h2>
-                                      </div>
-                                    <div class="card-body">
-                                      <p class="card-text" style="color:white"><font size="5">You have scored ${numCorrect} out of ${myQuestions.length}</p>
-                                    </div>
-                                  </div>
-                                  <br>
-                                    <h2 style="color:#47696b">Better Luck Next Time!!!!!</h2>
-                                  <div>`;
-      }
-      else{
+          <div class="card" style="width: 30rem; height:15rem">
+            <div class="card-header">
+              <h2>Result</h2>
+            </div>
+            <div class="card-body">
+              <p class="card-text" style="color:white"><font size="5">You have scored ${numCorrect} out of ${myQuestions.length}</p>
+            </div>
+          </div>
+          <br>
+          <h2 style="color:#47696b">Better Luck Next Time!!!!!</h2>
+        <div>`;
+      }else{
         quizContainer.innerHTML = `<div id="resultDisplay">
-                                    <div class="card" style="width: 30rem; height:15rem">
-                                      <div class="card-header">
-                                        <h3>Result</h3>
-                                      </div>
-                                    <div class="card-body">
-                                      <p class="card-text">You have scored ${numCorrect} out of ${myQuestions.length}</p>
-                                    </div>
-                                  </div>
-                                  <br>
-                                    <h2>Well Done!!!!!</h2>
-                                  <div>`;
+          <div class="card" style="width: 30rem; height:15rem">
+            <div class="card-header">
+              <h3>Result</h3>
+            </div>
+            <div class="card-body">
+              <p class="card-text">You have scored ${numCorrect} out of ${myQuestions.length}</p>
+            </div>
+            </div>
+            <br>
+            <h2>Well Done!!!!!</h2>
+        <div>`;
       }
 
       previousButton.style.display = "none";
@@ -119,16 +118,16 @@ function QuizStarted(){
       slides[n].classList.add("active-slide");
       currentSlide = n;
     
-      if (currentSlide === 0) {
+      if(currentSlide === 0){
         previousButton.style.display = "none";
-      } else {
+      }else{
         previousButton.style.display = "inline-block";
       }
     
       if (currentSlide === slides.length - 1) {
         nextButton.style.display = "none";
         submitButton.style.display = "inline-block";
-      } else {
+      }else{
         nextButton.style.display = "inline-block";
         submitButton.style.display = "none";
       }
@@ -151,8 +150,9 @@ function QuizStarted(){
         var name="question"+n,
             ele = document.getElementsByName(name);
 
-        for(var i=0;i<ele.length;i++)
+        for(var i=0;i<ele.length;i++){
           ele[i].checked = false;
+        }
       }
     }
 
@@ -166,33 +166,37 @@ function QuizStarted(){
     previousButton.addEventListener("click", showPreviousSlide);
     nextButton.addEventListener("click", showNextSlide);
 
-  document.getElementById("quizHeading").innerHTML=2 + ":" + 00;
-  document.getElementById("mainquizHeading").innerHTML = `Time left= ${document.getElementById("quizHeading").innerHTML}`;
-  startTimer();
+    document.getElementById("quizHeading").innerHTML=2 + ":" + 00;
+    document.getElementById("mainquizHeading").innerHTML = `Time left= ${document.getElementById("quizHeading").innerHTML}`;
 
-  function startTimer(myVar) {
-  myVar=setTimeout(startTimer, 1000);
-  console.log(myVar);
-  var presentTime = document.getElementById("quizHeading").innerHTML;
-  console.log(presentTime);
-  var timeArray = presentTime.split(/[:]+/);
-  console.log(timeArray);
-  var m = timeArray[0];
-  var s = checkSecond((timeArray[1] - 1));
-  if(s==59){m=m-1}
-  if(m<0){
-  clearTimeout(myVar);
-  showResults()
-  }  
-  document.getElementById('quizHeading').innerHTML = m + ":" + s;
-  document.getElementById("mainquizHeading").innerHTML = `Time left= ${document.getElementById("quizHeading").innerHTML}`;
-}
+    startTimer();
 
-function checkSecond(sec) {
-  if (sec < 10 && sec >= 0) {sec = "0" + sec};
-  if (sec < 0) {sec = "59"};
-  return sec;
-}
+    function startTimer(myVar) {
+      myVar=setTimeout(startTimer, 1000);
+      var presentTime = document.getElementById("quizHeading").innerHTML,
+          timeArray = presentTime.split(/[:]+/),
+          m = timeArray[0],
+          s = checkSecond((timeArray[1] - 1));
+      
+      if(s==59){
+        m=m-1;
+      }if(m<0){
+        clearTimeout(myVar);
+        showResults()
+      } 
+
+      document.getElementById('quizHeading').innerHTML = m + ":" + s;
+      document.getElementById("mainquizHeading").innerHTML = `Time left= ${document.getElementById("quizHeading").innerHTML}`;
+    }
+
+  function checkSecond(sec) {
+    if(sec < 10 && sec >= 0){
+      sec = "0" + sec;
+    }if(sec < 0){
+      sec = "59";
+    }
+    return sec;
+  }
 
   });           
 }
